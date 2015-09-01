4f917245ed51124caa534867017332e6e5be61cb
unknown <apeters@5VWLVZ1.ad.anteroresources.com>
2015-09-01 17:27:19 -0600

    Added Arduino Libraries

--- Arduino/MoistureSensor/MoistureSensor.ino
+++ Arduino/MoistureSensor/MoistureSensor.ino
@@ -1,8 +1,14 @@
-#include <SparkFunESP8266Client.h>
-#include <SparkFunESP8266Server.h>
+#include <SoftwareSerial.h>
+//#include <SparkFunESP8266Client.h>
+//#include <SparkFunESP8266Server.h>
 #include <SparkFunESP8266WiFi.h>
 
+String myName = "Moisture Sensor 1";
+String address = "10.0.0.146";
+int port = 3000;
+
 void setup() {
+  
   if(esp8266.begin()){
     Serial.println("ESP8266 available");
   }else{
@@ -13,39 +19,35 @@ void setup() {
     Serial.print("Error connecting to network: "+(String)conValue);
   }
 
-  ESP8266Client client;
   
-  // put your setup code here, to run once:
-    if (client.connect()) {
-    Serial.println("connected");
-  client.println("POST /tinyFittings/index.php HTTP/1.1");
-  client.println("Host:  artiswrong.com");
-  client.println("User-Agent: Arduino/1.0");
-  client.println("Connection: close");
-  client.println("Content-Type: application/x-www-form-urlencoded;");
-  client.print("Content-Length: ");
-  client.println(PostData.length());
-  client.println();
-  client.println(PostData);
-  } else {
-    Serial.println("connection failed");
-  }
-}
+  
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  int sensorData = analogRead(A0);
+  String postData = "{\"Name\":"+myName+
+                    ",\"Data\":"+(String)sensorData+
+                    "}";
+  String request = buildPostRequest("/logSensor",address,postData);
+  Serial.print(request);
+  ESP8266Client client;
+  int retVal = client.connect(address, port); // Connect to sparkfun (HTTP port)
+  if (retVal > 0) Serial.println("Successfully connected!");
+  client.print(request);
   
 }
 
-String buildPostRequest(String address, String host, String PostData ){
+String buildPostRequest(String address, String host, String postData ){
   String postRequest = "POST "+address+" HTTP/1.1\n";
-  postRequest += "Host: " + host+"\n";
+  postRequest += "Host: "+host+"\n";
   postRequest += "Connection: close\n";
-  postRequest += "Content-Type: application/json\n"
-  postRequest += "Content-Length: " +(String)PostData.length+"\n";
-  postRequest += "Content-Length
-  
+  postRequest += "Content-Type: application/json\n";
+  postRequest += "Content-Length: ";
+  postRequest += (String)postData.length()+"\n\n";
+  postRequest += postData +"\n\n";
+  return postRequest;
 }
 
 
--- GardenManager/app.js
+++ GardenManager/app.js
@@ -5,7 +5,7 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var sensorRoute = require('./routes/sensoerLog');
+var sensorRoute = require('./routes/sensorLog');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
